f945c47d8ffce1ae66e7c5dafd2bdc31b9a42dfb
Stajyer <stajyer@tp.com>
2026-08-07 16:53:00 +0300

    style: make email composer full screen and bump cache version

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'odakposta-v4';
-const DYNAMIC_CACHE = 'odakposta-dynamic-v4';
+const CACHE_NAME = 'odakposta-v5';
+const DYNAMIC_CACHE = 'odakposta-dynamic-v5';
 
 const STATIC_ASSETS = [
   '/',
